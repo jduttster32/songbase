7e9f62a6b941a00ce216a57e7841b054c824b1e7
James Dutton <Jdutt@udel.edu>
2017-11-09 13:48:20 -0500

    working with templates and multiple pages

--- songbase.py
+++ songbase.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to James first server. Thanks for checking it out.</h1>'
+    #return '<h1>Welcome to James first server. Thanks for checking it out.</h1>'
+    return render_template('index.html')
+
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+        'Free Fall',
+        'Sound of Walking Away',
+        'Feel Good'
+    ]
+    return render_template('songs.html', songs=songs)
+
 
 @app.route('/user/<string:name>')
 def get_user(name):
-    return '<h1>hello %s</h1>' % name
+    #return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    return render_template('user.html', user_name=name)
+
 
 if __name__ == "__main__":
     app.run(debug=True);
