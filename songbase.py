0c8f41f18e393620f520aa7f6d8b1146ec15fae0
James Dutton <Jdutt@udel.edu>
2017-11-14 13:53:03 -0500

    Added forms using GET and POST methods

--- songbase.py
+++ songbase.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +6,19 @@ def home():
     #return '<h1>Welcome to James first server. Thanks for checking it out.</h1>'
     return render_template('index.html')
 
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            return render_template('form-demo.html', first_name=session.get('first_name'))
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
 
 @app.route('/songs')
 def get_all_songs():
@@ -21,6 +34,8 @@ def get_all_songs():
 def get_user(name):
     #return '<h1>hello %s your age is %d</h1>' % (name, 3)
     return render_template('user.html', user_name=name)
+
+
 
 
 if __name__ == "__main__":
