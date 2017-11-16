5474da4152aa8ac5e2e2cc0fb644141fd7295751
James Dutton <Jdutt@udel.edu>
2017-11-16 13:40:25 -0500

    finished website

--- songbase.py
+++ songbase.py
@@ -1,10 +1,132 @@
+import os
 from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+    songs = db.relationship('Song', backref='artist')
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    lyrics = db.Column(db.Text)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+
 
 @app.route('/')
-def home():
-    #return '<h1>Welcome to James first server. Thanks for checking it out.</h1>'
+def index():
+    # return HTML
+    # return "<h1>this is the index page!<h1>"
     return render_template('index.html')
+
+
+@app.route('/artists')
+def show_all_artists():
+    artists = Artist.query.all()
+    return render_template('artist-all.html', artists=artists)
+
+
+@app.route('/artist/add', methods=['GET', 'POST'])
+def add_artists():
+    if request.method == 'GET':
+        return render_template('artist-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        about = request.form['about']
+
+        # insert the data into the database
+        artist = Artist(name=name, about=about)
+        db.session.add(artist)
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
+
+
+@app.route('/artist/edit/<int:id>', methods=['GET', 'POST'])
+def edit_artist(id):
+    artist = Artist.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('artist-edit.html', artist=artist)
+    if request.method == 'POST':
+        # update data based on the form data
+        artist.name = request.form['name']
+        artist.about = request.form['about']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
+
+
+# song-all.html adds song id to the edit button using a hidden input
+@app.route('/songs')
+def show_all_songs():
+    songs = Song.query.all()
+    return render_template('song-all.html', songs=songs)
+
+
+@app.route('/song/add', methods=['GET', 'POST'])
+def add_songs():
+    if request.method == 'GET':
+        artists = Artist.query.all()
+        return render_template('song-add.html', artists=artists)
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        year = request.form['year']
+        lyrics = request.form['lyrics']
+        artist_name = request.form['artist']
+        artist = Artist.query.filter_by(name=artist_name).first()
+        song = Song(name=name, year=year, lyrics=lyrics, artist=artist)
+
+        # insert the data into the database
+        db.session.add(song)
+        db.session.commit()
+        return redirect(url_for('show_all_songs'))
+
+
+@app.route('/song/edit/<int:id>', methods=['GET', 'POST'])
+def edit_song(id):
+    song = Song.query.filter_by(id=id).first()
+    artists = Artist.query.all()
+    if request.method == 'GET':
+        return render_template('song-edit.html', song=song, artists=artists)
+    if request.method == 'POST':
+        # update data based on the form data
+        song.name = request.form['name']
+        song.year = request.form['year']
+        song.lyrics = request.form['lyrics']
+        artist_name = request.form['artist']
+        artist = Artist.query.filter_by(name=artist_name).first()
+        song.artist = artist
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_songs'))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/users')
+def show_all_users():
+    return render_template('user-all.html')
+
 
 @app.route('/form-demo', methods=['GET', 'POST'])
 def form_demo():
@@ -20,23 +142,28 @@ def form_demo():
         # return render_template('form-demo.html', first_name=first_name)
         return redirect(url_for('form_demo'))
 
-@app.route('/songs')
-def get_all_songs():
-    songs = [
-        'Free Fall',
-        'Sound of Walking Away',
-        'Feel Good'
-    ]
-    return render_template('songs.html', songs=songs)
+
+@app.route('/user/<string:name>/')
+def get_user_name(name):
+    # return "hello " + name
+    # return "Hello %s, this is %s" % (name, 'administrator')
+    return render_template('user.html', name=name)
 
 
-@app.route('/user/<string:name>')
-def get_user(name):
-    #return '<h1>hello %s your age is %d</h1>' % (name, 3)
-    return render_template('user.html', user_name=name)
+@app.route('/song/<int:id>/')
+def get_song_id(id):
+    # return "This song's ID is " + str(id)
+    return "Hi, this is %s and the song's id is %d" % ('administrator', id)
 
 
+# https://goo.gl/Pc39w8 explains the following line
+if __name__ == '__main__':
 
+    # activates the debugger and the reloader during development
+    # app.run(debug=True)
+    app.run()
 
-if __name__ == "__main__":
-    app.run(debug=True);
+    # make the server publicly available on port 80
+    # note that Ports below 1024 can be opened only by root
+    # you need to use sudo for the following conmmand
+    # app.run(host='0.0.0.0', port=80)
